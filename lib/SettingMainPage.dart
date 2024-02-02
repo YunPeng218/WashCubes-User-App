1f2d8c62d4698176d3f40ad1d8b0c00921e167fd
RainieLai429 <laiyuer429@gmail.com>
2024-02-02 15:48:18 +0800

    halo

--- lib/SettingMainPage.dart
+++ lib/SettingMainPage.dart
@@ -1,4 +1,9 @@
+import 'package:device_run_test/FeedbackPage.dart';
+import 'package:device_run_test/PolicyPage.dart';
 import 'package:flutter/material.dart';
+
+import 'EditProfilePage.dart';
+import 'FAQsPage.dart';
 
 class SettingMainPage extends StatelessWidget {
   @override
@@ -16,16 +21,58 @@ class SettingMainPage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           UserHeader(),
-          ProfileOption(title: 'Edit Profile', icon: Icons.edit),
+          ProfileOption(
+            title: 'Edit Profile',
+            icon: Icons.edit,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => EditProfilePage()),
+              );
+            },
+          ),
           //ProfileOption(title: 'Referral', icon: Icons.card_giftcard),
           //ProfileOption(title: 'Rewards', icon: Icons.star),
-          ProfileOption(title: 'Settings', icon: Icons.settings),
-          ProfileOption(title: 'Customer Support', icon: Icons.support),
-          ProfileOption(title: 'Feedback Ratings', icon: Icons.feedback),
-          ProfileOption(title: 'About i3Cubes', icon: Icons.info_outline),
-          ProfileOption(title: 'FAQs', icon: Icons.question_answer),
-          //ProfileOption(title: 'Terms & Conditions', icon: Icons.description),
-          ProfileOption(title: 'Privacy Policy', icon: Icons.privacy_tip),
+          // ProfileOption(
+          //   title: 'Settings',
+          //   icon: Icons.settings,
+          //   onTap: () {
+          //     Navigator.push(
+          //       context,
+          //       MaterialPageRoute(builder: (context) => EditProfilePage()),
+          //     );
+          //   },
+          // ),
+          ProfileOption(
+            title: 'Feedback Ratings',
+            icon: Icons.feedback,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => FeedbackRatingsPage()),
+              );
+            },
+          ),
+          ProfileOption(
+            title: 'FAQs',
+            icon: Icons.question_answer,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => FAQsPage()),
+              );
+            },
+          ),
+          ProfileOption(
+            title: 'Privacy Policy',
+            icon: Icons.privacy_tip,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PolicyPage()),
+              );
+            },
+          ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -67,7 +114,7 @@ class ProfileOption extends StatelessWidget {
   final String title;
   final IconData icon;
 
-  const ProfileOption({Key? key, required this.title, required this.icon}) : super(key: key);
+  const ProfileOption({Key? key, required this.title, required this.icon, required Null Function() onTap}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
