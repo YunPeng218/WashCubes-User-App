46ee5436eabc15d092439ca0ea0646d7cdd815a7
YunPeng218 <yunpeng021802@hotmail.com>
2024-02-10 02:24:54 +0800

    Implemented profile page for guest

--- lib/src/features/screens/setting/account_screen.dart
+++ lib/src/features/screens/setting/account_screen.dart
@@ -7,8 +7,10 @@ import 'package:device_run_test/src/features/screens/chatbot/chatbotScreen.dart'
 import 'package:device_run_test/src/features/screens/notification/notification_screen.dart';
 import 'package:device_run_test/src/features/screens/setting/setting_screen.dart';
 import 'package:device_run_test/src/features/screens/welcome/welcome_screen.dart';
+import 'package:device_run_test/src/utilities/guest_mode.dart';
 import 'package:device_run_test/src/utilities/theme/widget_themes/text_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'edit_profile_screen.dart';
 import 'faq_screen.dart';
@@ -21,7 +23,7 @@ class AccountPage extends StatefulWidget {
   _AccountPageState createState() => _AccountPageState();
 }
 
-class _AccountPageState extends State <AccountPage> {
+class _AccountPageState extends State<AccountPage> {
   UserProfile? user;
 
   @override
@@ -52,122 +54,171 @@ class _AccountPageState extends State <AccountPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        //Notification Button
-        leading: IconButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const NotificationScreen()),
-            );
-          },
-          icon: const Icon(Icons.notifications_none),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(cDefaultSize - 10),
-        child: ListView(
-          children: <Widget>[
-            if (user != null)
-              UserHeader(user: user),
-            ProfileOption(
-              title: 'Edit Profile',
-              icon: Icons.person_outline_rounded,
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => EditProfilePage()),
-                ).whenComplete(() => loadUserInfo());
-              },
-            ),
-            const Divider(),
-            ProfileOption(
-              title: 'Settings',
-              icon: Icons.settings_outlined,
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SettingPage()),
-                );
-              },
-            ),
-            const Divider(),
-            ProfileOption(
-              title: 'Feedback Ratings',
-              icon: Icons.star_border_rounded,
-              onTap: () {
+    return Consumer<GuestModeProvider>(
+      builder: (context, guestProvider, child) {
+        return Scaffold(
+          appBar: AppBar(
+            // Notification Button
+            leading: IconButton(
+              onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => const FeedbackRatingsPage()),
-                );
-              },
-            ),
-            const Divider(),
-            ProfileOption(
-              title: 'FAQs',
-              icon: Icons.question_answer,
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FAQsPage()),
-                );
-              },
-            ),
-            const Divider(),
-            ProfileOption(
-              title: 'Privacy Policy',
-              icon: Icons.shield_outlined,
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const PolicyPage()),
-                );
-              },
-            ),
-            const Divider(),
-            ProfileOption(
-              title: 'Logout',
-              icon: Icons.logout,
-              onTap: () async {
-                SharedPreferences prefs = await SharedPreferences.getInstance();
-                await prefs.clear();
-                Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(
-                    builder: (context) => const WelcomeScreen(),
+                    builder: (context) => const NotificationScreen(),
                   ),
-                  (Route<dynamic> route) => false,
                 );
               },
+              icon: const Icon(Icons.notifications_none),
             ),
-            const Divider(),
-          ],
-        ),
-      ),
-      //ChatBot Trimi Button
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const ChatbotScreen()),
-          );
-        },
-        tooltip: 'Increment',
-        child: Image.asset(cChatBotLogo),
-      ),
-      //BottomNavBar
-      bottomNavigationBar: const BottomNavBar(),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(cDefaultSize - 10),
+            child: guestProvider.guestMode
+                ? Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'Sign In to View Your Profile',
+                            style: CTextTheme.blackTextTheme.headlineSmall,
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 10),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const WelcomeScreen(),
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              foregroundColor: AppColors.cBlackColor,
+                              backgroundColor: AppColors.cWhiteColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                side: const BorderSide(color: AppColors.cGreyColor1),
+                              ),
+                            ),
+                            child: Text(
+                              'Sign In',
+                              style: CTextTheme.blackTextTheme.labelMedium,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                : ListView(
+                    children: <Widget>[
+                      if (user!=null)
+                        UserHeader(user: user),
+                      ProfileOption(
+                        title: 'Edit Profile',
+                        icon: Icons.person_outline_rounded,
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => EditProfilePage()),
+                          ).whenComplete(() => loadUserInfo());
+                        },
+                      ),
+                      const Divider(),
+                      ProfileOption(
+                        title: 'Settings',
+                        icon: Icons.settings_outlined,
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SettingPage()),
+                          );
+                        },
+                      ),
+                      const Divider(),
+                      ProfileOption(
+                        title: 'Feedback Ratings',
+                        icon: Icons.star_border_rounded,
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const FeedbackRatingsPage()),
+                          );
+                        },
+                      ),
+                      const Divider(),
+                      ProfileOption(
+                        title: 'FAQs',
+                        icon: Icons.question_answer,
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => FAQsPage()),
+                          );
+                        },
+                      ),
+                      const Divider(),
+                      ProfileOption(
+                        title: 'Privacy Policy',
+                        icon: Icons.shield_outlined,
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const PolicyPage()),
+                          );
+                        },
+                      ),
+                      const Divider(),
+                      ProfileOption(
+                        title: 'Logout',
+                        icon: Icons.logout,
+                        onTap: () async {
+                          SharedPreferences prefs =
+                              await SharedPreferences.getInstance();
+                          await prefs.clear();
+                          Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(
+                              builder: (context) => const WelcomeScreen(),
+                            ),
+                            (Route<dynamic> route) => false,
+                          );
+                        },
+                      ),
+                      const Divider(),
+                    ],
+                  ),
+          ),
+          // ChatBot Trimi Button
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ChatbotScreen()),
+              );
+            },
+            tooltip: 'Increment',
+            child: Image.asset(cChatBotLogo),
+          ),
+          // BottomNavBar
+          bottomNavigationBar: const BottomNavBar(),
+        );
+      },
     );
   }
 }
 
 class UserHeader extends StatelessWidget {
-  UserProfile? user;
+  final UserProfile? user;
 
-  UserHeader({Key? key, required this.user})
-      : super(key: key); 
+  UserHeader({Key? key, required this.user}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -176,15 +227,10 @@ class UserHeader extends StatelessWidget {
         backgroundImage: NetworkImage(user!.profilePicURL),
         radius: 30,
       ),
-      title: user!.name.isNotEmpty
-        ? Text(
-          user!.name,
-          style: CTextTheme.blackTextTheme.displaySmall,
-        )
-        : Text(
-          'Trimi',
-          style: CTextTheme.blackTextTheme.displaySmall,
-        ),
+      title: Text(
+        user?.name.isNotEmpty ?? false ? user!.name : 'Trimi',
+        style: CTextTheme.blackTextTheme.displaySmall,
+      ),
     );
   }
 }
